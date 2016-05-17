46b5103695dc83a215a623a4c1c112e650b790d4
rocbird <316605802@qq.com>
2016-05-17 21:03:04 +0800

    更新resume内容、样式、脚本

--- date/main.js
+++ date/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-	var isBool;
 	$(".m-skill-box").find(".skill-box").click(function() {
+		var isBool;
 		var isBoolF = $(".m-skill-html").is(":animated") || $(".m-skill-css").is(":animated");/* 第一层盒子动画检测 */
 		var isBoolS = $(".m-skill-js").is(":animated") || $(".m-skill-ps").is(":animated");/* 第二层盒子动画检测 */
 		var sBox = $(this).closest(".skill-box")[0];/* 被点击对象所在skill-box（即s盒子） */
@@ -15,7 +15,6 @@ $(document).ready(function() {
 		if ($(sBox).index() === 0) {
 			if ($(sBoxB).css("display") !== "none") {
 				if (!isBool) {
-					$(sBoxB).css("zIndex","5");
 					$(sBoxS).show();
 					$(sBox).css({"borderColor":"#ccc","borderRightStyle":"dashed"});
 					$(sBoxS).animate({"left":"300px"},800);
@@ -24,16 +23,13 @@ $(document).ready(function() {
 			} else {
 				if (!isBool) {
 					$(sBoxB).show();
-					// $(sBox).css({"borderColor":"transparent","borderRightStyle":"solid","hover":""});
-					//$(sBox).hover({"borderColor":"#e4e4e4"});
 					$(sBoxS).animate({"left":"0"},800,function(){$(this).hide();$(sBox).removeAttr('style');});
-					$(sBoxB).animate({"left":"300px"},800,function(){$(this).css("zIndex","10");});
+					$(sBoxB).animate({"left":"300px"},800);
 				}
 			}
 		} else if ($(sBox).index() === 3) {
 			if ($(sBoxS).css("display") !== "none") {
 				if (!isBool) {
-					$(sBoxS).css("zIndex","5");
 					$(sBoxB).show();
 					$(sBox).css({"borderColor":"#ccc","borderLeftStyle":"dashed"});
 					$(sBoxB).animate({"left":"0"},800);
@@ -43,59 +39,54 @@ $(document).ready(function() {
 				if (!isBool) {
 					$(sBoxS).show();
 					$(sBoxB).animate({"left":"300px"},800,function(){$(this).hide();$(sBox).removeAttr('style');});
-					$(sBoxS).animate({"left":"0"},800,function(){$(this).css("zIndex","10");});
+					$(sBoxS).animate({"left":"0"},800);
 				}
 			}
 		}
-	});
+	});/* 技能展示页点击动画 */
+
 	$(".modular-m-f-icon").click(function() {
 		if (!$(".modular-m-f-notes").is(":animated")) {
 			console.log($(".modular-m-f-notes").css("left"));
 			$(".modular-m-f-notes").fadeToggle(1500,"linear");
 		}
-	});
-	$(".m-toggle-b-down").click(function(){
-		var modularsNone;
+	});/* 底部读白显、隐 */
+
+	$(".m-toggle-b-up").css("background","#e4e4e4");
+	$(".modular-toggle-button").children().click(function(){
 		var modulars = $(".modular-main").closest(".modular-main");
-		console.log(modulars.length);
-		modulars.each(function(index,element){
-			if(!$(this).is(".dis-none")) {
-				modularsNone = $(this);
-				console.log(modularsNone.next().index());
-			}
-		});
-		if (modularsNone != undefined) {
-			if (modularsNone.index() < modulars.length){
-				modularsNone.addClass("dis-none");
-				modularsNone.next().removeClass("dis-none");
-				if (modularsNone.index() == modulars.length-1) {
-					$(this).css("background","#e4e4e4");
-				} else {
-					$(".m-toggle-b-up").removeAttr("style");
+		if (!modulars.is(":animated")) {
+			var modularSingle;
+			modulars.each(function(){
+				if($(this).css("display") !== "none") {
+					modularSingle = $(this);
+				}
+			});
+			if (modularSingle != undefined) {
+				if ($(this).is(".m-toggle-b-up")) {
+					if (modularSingle.index() >= 2){
+						modularSingle.prev().show();
+						modularSingle.css({"zIndex":"55","background":"rgba(0,0,0,.2)"});
+						modularSingle.animate({"left":"722px"},1200,function(){$(this).hide();$(this).removeAttr("style");});
+						if (modularSingle.index() == 2) {
+							$(this).css("background","#e4e4e4");
+						} else {
+							$(".m-toggle-b-down").removeAttr("style");
+						}
+					}
+				} else if ($(this).is(".m-toggle-b-down")) {
+					if (modularSingle.index() <= modulars.length-1){
+						modularSingle.next().show();
+						modularSingle.css({"zIndex":"55","background":"rgba(0,0,0,.2)"});
+						modularSingle.animate({"left":"-722px"},1200,function(){$(this).hide();$(this).removeAttr("style");});
+						if (modularSingle.index() == modulars.length-1) {
+							$(this).css("background","#e4e4e4");
+						} else {
+							$(".m-toggle-b-up").removeAttr("style");
+						}
+					}
 				}
 			}
 		}
-	});
-	$(".m-toggle-b-up").click(function(){
-		var modularsNone;
-		var modulars = $(".modular-main").closest(".modular-main");
-		console.log(modulars.length);
-		modulars.each(function(index,element){
-			if(!$(this).is(".dis-none")) {
-				modularsNone = $(this);
-				console.log(modularsNone.prev().index());
-			}
-		});
-		if (modularsNone != undefined) {
-			if (modularsNone.index() > 1){
-				modularsNone.addClass("dis-none");
-				modularsNone.prev().removeClass("dis-none");
-				if (modularsNone.index() == 2) {
-					$(this).css("background","#e4e4e4");
-				} else {
-					$(".m-toggle-b-down").removeAttr("style");
-				}
-			}
-		}
-	});
+	});/* 模块切换 */
 });
